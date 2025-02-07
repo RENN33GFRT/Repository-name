9db7803ef2b97f315a6b920125226f7a046c6d1c
Kvartalnyy.A <alexeyderzinov553@gmail.com>
2025-02-07 10:24:33 +0300

    rewrite processing file

--- src/processing.py
+++ src/processing.py
@@ -1,30 +1,37 @@
-def mask_account_card(account_info: str) -> str:
-    """
-    Маскирует номер карты или счета, показывая только часть информации.
-    """
-    last_index = 0
-
-    for i in range(len(account_info)):
-        if account_info[i] == ' ':
-            last_index = i
-
-    card_type = account_info[:last_index]
-    card_number = account_info[last_index + 1:]
-
-    if 'Счет' in card_type:
-        masked_number = f"{card_type} **{card_number[-4:]}"
-    else:
-        masked_number = f"{card_type} {card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
-
-    return masked_number
+from typing import Any
 
 
-def get_date(date_str: str) -> str:
-    """
-    Преобразует строку даты в формат ДД.ММ.ГГГГ.
-    """
-    year = date_str[0:4]
-    month = date_str[5:7]
-    day = date_str[8:10]
+def filter_by_state(list_of_dicts: list[Any], state: str = "EXECUTED") -> list[Any]:
+    """Проверяет значение в словарях на заданное и если оно совпадает то, выводит его"""
+    correct_list = []
+    for num_dict in range(0, len(list_of_dicts) - 1):
+        if list_of_dicts[num_dict]["state"] == state:
+            correct_list.append(list_of_dicts[num_dict])
+    return correct_list
 
-    return f"{day}.{month}.{year}"
+
+def sort_by_date(list_of_dicts: list[Any], sorting_direct: bool = True) -> list[Any]:
+    """Сортирует список по датам в словарях по убывание(по умолчанию)"""
+    return sorted(list_of_dicts, key=lambda x: x.get("date"), reverse=sorting_direct)
+
+
+print(
+    filter_by_state(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
+print(
+    sort_by_date(
+        [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
