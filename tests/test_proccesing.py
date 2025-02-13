d71ed4954d517c19d2447851f355f8be02a9ffb2
Kvartalnyy.A <alexeyderzinov553@gmail.com>
2025-02-13 13:45:38 +0300

    README, tests fix

--- tests/test_proccesing.py
+++ tests/test_proccesing.py
@@ -1,29 +1,19 @@
 import pytest
 
-from src import masks
+from src import processing
 
 
-@pytest.mark.parametrize(
-    "numbers_info,expected",
-    [
-        ("1596837868705199", "1596 83** **** 5199"),
-        ("6831982476737658", "6831 98** **** 7658"),
-        ("897304958f7290kj", "Error"),
-        ("394075", "Error"),
-    ],
-)
-def test_mask_card_number(numbers_info, expected):
-    assert masks.get_mask_card_number(numbers_info) == expected
+def test_filter_by_state(operations_info):
+    assert processing.filter_by_state(operations_info) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
 
 
-@pytest.mark.parametrize(
-    "numbers,expected",
-    [
-        ("73654108430135874305", "**4305"),
-        ("19861059860492865092", "**5092"),
-        ("saigq54298dfgjkh895h", "Error"),
-        ("23576908536", "Error"),
-    ],
-)
-def test_get_mask_account(numbers, expected):
-    assert masks.get_mask_account(numbers) == expected
+def test_sort_by_date(operations_info):
+    assert processing.sort_by_date(operations_info) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
