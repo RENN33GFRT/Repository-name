4f2136662c2bdbb1b53de7bfe90452231e2e5e6a
Kvartalnyy.A <alexeyderzinov553@gmail.com>
2025-02-24 15:53:10 +0300

    Create 2 new functions decorators and test_decorators

---
+++ src/decorators.py
@@ -0,0 +1,41 @@
+# decorators.py
+import logging
+import sys
+from functools import wraps
+
+
+def setup_logger(filename: object = None) -> object:
+    logger = logging.getLogger('FunctionLogger')
+    logger.setLevel(logging.DEBUG)
+
+    if filename:
+        handler = logging.FileHandler(filename)
+    else:
+        handler = logging.StreamHandler(sys.stdout)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+
+    return logger
+
+
+def log(filename=None):
+    logger = setup_logger(filename)
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            logger.info(f'Starting function: {func.__name__} with arguments: {args}, {kwargs}')
+            try:
+                result = func(*args, **kwargs)
+                logger.info(f'Function: {func.__name__} completed successfully with result: {result}')
+                return result
+            except Exception as e:
+                logger.error(
+                    f'Function: {func.__name__} raised an error: {type(e).__name__} - {e} with arguments: {args}, {kwargs}')
+                raise  # повторно выбрасываем исключение после логирования
+
+        return wrapper
+
+    return decorator
---
+++ tests/test_decorators.py
@@ -0,0 +1,47 @@
+import pytest
+import os
+import logging
+
+
+# Удаляем файл лога, если он существует
+LOGFILE = 'test_logfile.log'
+if os.path.exists(LOGFILE):
+    os.remove(LOGFILE)
+
+# Пример функции для тестирования
+@log(LOGFILE)
+def add(a, b):
+    return a + b
+
+@log(LOGFILE)
+def divide(a, b):
+    return a / b
+
+def test_add():
+    result = add(5, 3)
+    assert result == 8
+
+def test_divide():
+    result = divide(10, 2)
+    assert result == 5
+
+def test_divide_by_zero():
+    with pytest.raises(ZeroDivisionError):
+        divide(10, 0)
+
+def test_log_file_exists():
+    assert os.path.exists(LOGFILE)
+
+def test_log_content():
+    with open(LOGFILE, 'r') as f:
+        log_content = f.read()
+        assert 'Starting function: add with arguments: (5, 3)' in log_content
+        assert 'Function: add completed successfully with result: 8' in log_content
+        assert 'Starting function: divide with arguments: (10, 2)' in log_content
+        assert 'Function: divide completed successfully with result: 5' in log_content
+        assert 'Function: divide raised an error: ZeroDivisionError' in log_content
+
+# Удаляем файл лога после тестов
+def teardown_module(module):
+    if os.path.exists(LOGFILE):
+        os.remove(LOGFILE)
